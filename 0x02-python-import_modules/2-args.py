e2f92c06802edbbeb7335a2fd576493bd534434e
kabute muthoni <kabutemuthoni@gmail.com>
2023-08-11 20:11:08 -0700

    modules and improts

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,6 @@ import sys
 argument = len(sys.argv) -1
 if argument == 0:
     print("0 arguments.")
-elif argument == 1:
-    print("1 argument.")
 else:
     print("{} arguments:".format(argument))
     for i in range(argument):
