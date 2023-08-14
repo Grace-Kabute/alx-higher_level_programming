44db07350474011dd21762105d67ed7dabcee3b4
kabute muthoni <kabutemuthoni@gmail.com>
2023-08-14 12:51:26 -0700

    data structures

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """prints the values of a list"""
-
+"""prints the values of a list"""
 def print_list_integer(my_list=[]):
     for i in my_list:
         print("{}".format(i))
