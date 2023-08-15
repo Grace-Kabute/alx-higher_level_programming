0aa9a011d9e8f76ae74a3b688238387a83052967
kabute muthoni <kabutemuthoni@gmail.com>
2023-08-14 18:44:08 -0700

    Data structure

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    """finds the largest int"""
+    if len(my_list) == 0:
+        return None
+    else:
+        for num in my_list:
+            if num > my_list[0]:
+                maximum = num
+                return maximum
