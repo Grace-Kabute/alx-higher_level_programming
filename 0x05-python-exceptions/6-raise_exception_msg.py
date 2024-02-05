dbf1eec6700bbbb12cb031fec447dcee4f2e5cf6
kabute muthoni <kabutemuthoni@gmail.com>
2024-02-05 12:57:42 -0800

    raise exception

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
     """raisees a name exception"""
-    try:
-        raise NameError
-
-    except NameError:
-        print("C is fun")
+        raise NameError(message)
