e339d807064a2bb98cb2d91094a48ef4448704ab
kabute muthoni <kabutemuthoni@gmail.com>
2024-02-19 01:07:10 -0800

    classes and objects

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """a class that defines a square"""
+
+
 class Square:
     """defines a square"""
-    __size = None
-    def __init__ (self,size):
+    def __init__(self, size="3"):
         """initialization with size"""
-
+        self.__size = size
