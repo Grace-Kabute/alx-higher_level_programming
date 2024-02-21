aa5f30bd48317eb5b6d7020586dbbd74af9b36cd
kabute muthoni <kabutemuthoni@gmail.com>
2024-02-21 02:30:22 -0800

    json

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """appends a string at the end of a text file"""
+
+
 def append_write(filename="", text=""):
     """appends a string at file and returns the number of characters"""
-    with open(filename, mode="a", encoding = "utf-8") as appends:
-       return appends.write(text)
-
+    with open(filename, mode="a", encoding="utf-8") as appends:
+        return appends.write(text)
