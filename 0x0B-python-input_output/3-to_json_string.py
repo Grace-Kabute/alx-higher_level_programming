7e85c300e7f10253a3a3d0bc45c4054cf4d87198
kabute muthoni <kabutemuthoni@gmail.com>
2024-02-21 02:15:21 -0800

    json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import json
 """returns JSON representation of object"""
+import json
+
 def to_json_string(my_obj):
     """JSON representation"""
     return json.dumps(my_obj)
