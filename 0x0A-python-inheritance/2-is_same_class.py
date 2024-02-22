e4595f3be1bb41f80db2f05f1b4ea53675a7fde8
kabute muthoni <kabutemuthoni@gmail.com>
2024-02-21 18:56:08 -0800

    inheritance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""returns True if the object an instance of a class"""
+def is_same_class(obj, a_class):
+    """checks instance of a class"""
+    if type(obj) == a_class:
+        return True
+    return False
