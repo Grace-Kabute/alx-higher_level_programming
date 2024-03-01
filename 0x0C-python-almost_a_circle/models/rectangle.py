a26d1fc4bdc09395406fbfb2f13b3b52643df566
kabute <kabutemuthoni@gmail.com>
2024-03-01 10:36:44 -0800

    almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,15 +76,49 @@ class Rectangle(Base):
 
     def display(self):
         """prints the stdout of the rectangle"""
-        for _ in range(self.__height):
-            print('#' * self.__width)
         for _ in range(self.__y):
-            print('#' * self.__x)
-        for _ in range(self.__x):
-            print('#' * self.__y)
+            print()
+        for _ in range(self.__height):
+            print(" " * self.__x + '#' * self.__width)
 
     def __str__(self):
         """magic method"""
         return "[{}] ({}) {}/{} - {}/{}".format(type(self).__name__, self.id,
                                                 self.x, self.y, self.width,
                                                 self.height)
+
+    def update(self, *args, **kwargs):
+        """assign aguments"""
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
